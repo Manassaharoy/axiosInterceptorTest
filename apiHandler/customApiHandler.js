ad766d3142b3da6c0126119ca25c3c8b3042cd14
Manassaharoy <triptoroy5@gmail.com>
2023-05-30 18:38:43 +0600

    updated put req

---
+++ apiHandler/customApiHandler.js
@@ -0,0 +1,92 @@
+import axios from "axios";
+import caxios from "@/apiHandler/axiosInstance";
+import decryptData from "./utils/decryption";
+
+export const getRequestHandler = async (url) => {
+  let responseData = await caxios.get(url);
+  return responseData;
+};
+
+export const postRequestHandler = async (url, bodyData) => {
+  let responseData = await caxios.post(url, { ...bodyData });
+  return responseData;
+};
+
+export const patchRequestHandler = async () => {};
+
+export const putRequestHandler = async (formData) => {
+  const accessToken = localStorage.getItem("accessToken");
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "multipart/form-data",
+  };
+
+  let responseData;
+  await axios
+    .put(
+      `${process.env.NEXT_PUBLIC_APIPOINT}/user/profile/uploadpicture`,
+      formData,
+      {
+        headers,
+      }
+    )
+    .then((response) => {
+      responseData = JSON.parse(decryptData(responseData.data.encoded));
+    })
+    .catch((error) => {
+      responseData = JSON.parse(decryptData(error.response.data.encoded));
+    });
+
+  return responseData;
+};
+
+export const deleteRequestHandler = async () => {};
+
+export const loginHandler = async (url, phoneNumber, password) => {
+  let responseData = await postRequestHandler(url, {
+    authorization: "application:secret",
+    grant_type: "password",
+    phoneNumber: phoneNumber,
+    password: password,
+  });
+
+  if (responseData.success) {
+    localStorage.setItem("accessToken", responseData.data.accessToken);
+    localStorage.setItem(
+      "accessTokenExpiresAt",
+      responseData.data.accessTokenExpiresAt
+    );
+    localStorage.setItem("refreshToken", responseData.data.refreshToken);
+    localStorage.setItem(
+      "refreshTokenExpiresAt",
+      responseData.data.refreshTokenExpiresAt
+    );
+    localStorage.setItem("user", responseData.data.user);
+    localStorage.setItem("createrAt", responseData.data.createrAt);
+  }
+
+  return responseData;
+};
+
+export const logoutHandler = async (url) => {
+  let responseData = await postRequestHandler(url, { data: "" });
+
+  if (responseData.success) {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("accessTokenExpiresAt");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("refreshTokenExpiresAt");
+    localStorage.removeItem("user");
+    localStorage.removeItem("createrAt");
+  }
+
+  return responseData;
+};
+
+//SAMPLE REQUEST WITH AUTHORIZATION TOKEN
+export const authGetRequestHandler = async (url) => {
+  caxios.defaults.headers.common["Authorization"] =
+    "Bearer fc95e87e0a205c5a77ba8b7b753b91b09e53da13";
+  let responseData = await caxios.get(url);
+  return responseData;
+};
--- app/page.js
+++ app/page.js
@@ -1,95 +1,134 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import {
+  getRequestHandler,
+  loginHandler,
+  logoutHandler,
+  postRequestHandler,
+  putRequestHandler,
+} from "@/apiHandler/customApiHandler";
+import decryptData from "@/apiHandler/utils/decryption";
+import { headers } from "@/next.config";
+import { useState } from "react";
 
 export default function Home() {
+  const [response, setResponse] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const checkGet = async () => {
+    let data = await getRequestHandler("/");
+    console.log("get request ----- ", data);
+    setResponse(data);
+  };
+
+  const checkPost = async () => {
+    let data = await postRequestHandler("/auth/signup", {
+      email: "manas@gmail.com",
+      phoneNumber: "01515212628",
+      password: "111111",
+    });
+    console.log("post request ----- ", data);
+    setResponse(data);
+  };
+
+  const checkLogin = async () => {
+    let url = "/auth/login";
+    let phoneNumber = "01515212628";
+    let password = "111111";
+
+    let data = await loginHandler(url, phoneNumber, password);
+    console.log("post request ----- ", data);
+    setResponse(data);
+  };
+
+  const checkAuth = async () => {
+    let data = await getRequestHandler("/auth/authcheck");
+    console.log("get request ----- ", data);
+    setResponse(data);
+  };
+  const checkLogout = async () => {
+    let data = await logoutHandler("/auth/logout");
+    console.log("get request ----- ", data);
+    setResponse(data);
+  };
+
+  const uploadAnImage = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("profilePhoto", selectedFile);
+
+      // const accessToken = localStorage.getItem("accessToken");
+      // const headers = {
+      //   Authorization: `Bearer ${accessToken}`,
+      //   "Content-Type": "multipart/form-data",
+      // };
+      
+      // console.log("formData ----- ", formData);
+
+      // let data = await axios
+      //   .put("https://marpapi.techanalyticaltd.com/user/profile/uploadpicture", formData, {
+      //     headers,
+      //   })
+      //   .then((response) => {
+      //     let responseData = JSON.parse(decryptData(response.data.encoded));
+      //     // let responseData = response.data.encoded;
+      //     console.log("Upload successful");
+      //     console.log(responseData);
+      //   });
+
+      let data = await putRequestHandler(formData);
+
+      console.log("put request ----- ", data);
+      setResponse(data);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <main>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button onClick={checkGet}>get check</button>
+          <br />
+          <button onClick={checkPost}>signup check</button>
+          <br />
+          <button onClick={checkLogin}>login check</button>
+          <br />
+          <button onClick={checkAuth}>Auth check</button>
+          <br />
+          <button onClick={checkLogout}>Logout check</button>
+          <br />
+        </div>
+        <div
+          style={{
+            border: "1px solid black",
+            width: "80%",
+            height: "80vh",
+          }}
+        >
+          <button
+            onClick={() => {
+              setResponse("");
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            CLEAR
+          </button>
+          <h3>Response from database:</h3>
+          {response ? JSON.stringify(response) : ""}
+        </div>
+        <div>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={uploadAnImage}>Upload</button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
